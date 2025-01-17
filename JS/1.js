e6cbd868cb956338ff4b7a00eb1f95c448f3f859
MarkOvodov <ovodovmark13@gmail.com>
2025-01-17 19:58:15 +0300

    java script introduction

---
+++ JS/1.js
@@ -0,0 +1,38 @@
+console.log("first line in java script for me")
+
+let value
+let $money$
+let _50cent
+const pi = 3.14
+console.log(typeof (typeof 1))
+
+let success = true
+let question = false
+console.log(success, typeof success)
+console.log(question, typeof question)
+
+let drug = 228
+let percent = 0.34
+let not_a_number = NaN
+let space = Infinity
+console.log(drug, typeof drug)
+console.log(percent, typeof percent)
+console.log(not_a_number, typeof not_a_number)
+console.log(space, typeof space)
+
+let kadet = 'leha ivanov'
+let femboy = "egor voronov"
+let teacher = `german`
+console.log(kadet, typeof kadet)
+console.log(femboy, typeof femboy)
+console.log(teacher, typeof teacher)
+
+const e = 2.718281828459045
+let descriptionOf_e = `Число е равно ${e}`
+console.log(descriptionOf_e)
+
+let any
+console.log(typeof any)
+
+let sum = null
+console.log(sum, typeof sum)
