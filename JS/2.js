2a423c456061e072f87127294733258e3f582c6c
MarkOvodov <ovodovmark13@gmail.com>
2025-01-24 19:28:44 +0300

    string concatenation, math and logic operators

---
+++ JS/2.js
@@ -0,0 +1,19 @@
+let start = 'I have '
+let countOfPets = 2
+let finish = ' pets at home'
+
+let text = start + countOfPets + finish
+console.log(text)
+
+let anyValue = null
+console.log('real:', anyValue)
+console.log('Boolean:', Boolean(anyValue))
+console.log('Number:', Number(anyValue))
+console.log('String:', String(anyValue))
+
+// сокращенные операторы
+console.log(+'5')
+console.log(!!'5')
+console.log(!!0)
+
+console.log(null == null)
