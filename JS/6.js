44503e76a0e4abfd2d5a582d52f289f78d556d22
MarkOvodov <ovodovmark13@gmail.com>
2025-02-25 19:52:09 +0300

    objects with referances to ultrakill

---
+++ JS/6.js
@@ -0,0 +1,89 @@
+// dukty
+
+let machine = {
+    firmware: '2112.08.06',
+    machine_id: 'V1',
+    location: 'Approaching hell',
+    current_objective: 'Find a weapon',
+}
+
+
+
+let mankind = 'dead'
+let blood = 'fuel'
+let hell = 'full'
+
+delete machine.location
+
+machine.location = '7th layer: violence'
+
+console.log(machine)
+
+let characters = [
+    'v1',
+    'v2',
+    'gabriel',
+    'minos',
+    'sisiphys'
+]
+
+console.log(characters)
+
+let subjects = [
+    {
+        cgvhbjnkml: 'ssfsf',
+        cgvjhbkjnlkm: 'fsf'
+    },
+    {
+        sknk: '435678908786547',
+        jadvjv: '2443555'
+    }
+]
+
+console.log(subjects)
+console.log(Object.keys(machine))
+console.log(Object.values(machine))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //      /=======\
+    //     /  /===\  \
+    //    |  | 000 |  | 
+    //     \  \===/  /
+    //      \=======/
+    //         ||
+    //    /===========\
+    //    |    | | V1 |
+    //    \ ---| |--- /
+    //     \=========/
+    //      \|||||||/
+    //       \|||||/
+    //       \     /
+    //        \===/
